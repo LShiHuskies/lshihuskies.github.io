a2de845825ed22419fd2a495cdd7ebb99066d549
github username <github email address>
2018-11-02 16:57:48 -0400

    divBox

--- index.js
+++ index.js
@@ -54,9 +54,43 @@ const makeVerticalNavBar = () => {
 
 
 const handleDivBox = (event) => {
+  const aboutBox = document.getElementById('ABOUTME');
+  if (aboutBox !== null) {
+    aboutBox.remove();
+  }
   if (event.target.innerText === 'About Me') {
+
     const aboutBox = document.createElement('DIV');
-    aboutBox.setAttribute('id', )
+    aboutBox.setAttribute('id', 'ABOUTME');
+    // aboutBox.setAttribute('class', 'animated fadeInRight delay-0.5s')
+    // console.log(aboutBox);
+    const arrInfo = ['My name is Louis Shi', 'I am a software developer and I turn ideas into reality through programming',
+                      'I graduated from the University of Connecticut',
+                      'I worked in Insurance for a couple years handling Bodily Injury Claims for GEICO where I consistently produced one of the highest performance metrics in the company',
+                      'Through online programming tutorials and an Introduction of Computer Science Night course I discovered my passion for programming',
+                      'I enrolled in Flatiron School Software Engineering Immersive program where I continued to build various projects such as Z Warriors Action Game, Space Shooter App and Career Guidance App demonstrating strong skills in Ruby, Ruby on Rails, JavaScript, React, Redux, HTML, CSS, SQL, PostgreSQL and WebSocket',
+                    'I realize that throughout my journey being a developer has always been a natural extension of myself and that I have always been a developer',
+                  ];
+
+    arrInfo.forEach(element => { let pTag = document.createElement('P');
+
+      pTag.setAttribute('class', `animated fadeInRight delay-${arrInfo.indexOf(element)}s`);
+      if (arrInfo.indexOf(element) !== 6) {
+        pTag.innerText = element;
+        aboutBox.appendChild(pTag)
+      } else if (arrInfo.indexOf(element) === 6) {
+        setTimeout(function handleSix() {
+          let pTag = document.createElement('P');
+          pTag.setAttribute('class', `animated fadeInRight delay-${arrInfo.indexOf(element)}s`);
+          pTag.innerText = element;
+          aboutBox.appendChild(pTag)
+        }, 6000);
+      }
+      const divBox = document.getElementById('greetTwo');
+      divBox.appendChild(aboutBox);
+    } )
+
+
   } else if (event.target.innerText === 'Hobbies') {
 
   } else if (event.target.innerText === 'Technical') {
@@ -67,7 +101,13 @@ const handleDivBox = (event) => {
 }
 
 
-
+// function handleSix (arrInfo, element) {
+//   alert('here')
+//   let pTag = document.createElement('P');
+//   pTag.setAttribute('class', `animated fadeInRight delay-${arrInfo.indexOf(element)}s`);
+//   pTag.innerText = element;
+//   return pTag;
+// }
 
 
 
