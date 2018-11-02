f53e5bf37b310a7fd9edd6a4f02159e696b20bb2
github username <github email address>
2018-11-02 01:18:35 -0400

    projects

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-let counter = 0;
-let innerCounter = 0;
-const container = document.getElementById('happy');
-const button = document.getElementById('play');
+// let counter = 0;
+// let innerCounter = 0;
+// const container = document.getElementById('happy');
+// const button = document.getElementById('play');
+//
+// button.addEventListener('click', (event) => {
+//   // let rectangle = document.createElement('DIV');
+//   makeRectangle(event);
+//
+// })
+//
+// const makeRectangle = (event) => {
+//   counter++;
+//   const container = document.getElementById('happy');
+//
+//   const rectangle = document.createElement('DIV');
+//   rectangle.setAttribute('ID', `${counter}`);
+//   rectangle.setAttribute('CLASS', 'rectangle')
+//   container.appendChild(rectangle);
+//   rectangle.innerText = counter;
+// }
+//
+//
+// container.addEventListener('click', (event) => {
+//   innerCounter = 0;
+//   if (event.target.id !== 'happy') {
+//     // alert(event.target.id);
+//     const rec = document.getElementById(`${event.target.id}`);
+//     makeInnerRectangle(rec)
+//     // const innerRectangle = document.createElement('DIV');
+//     // innerRectangle.setAttribute('class', 'innerRectangle');
+//     // rec.appendChild(innerRectangle);
+//   }
+// })
+//
+// const makeInnerRectangle = (rec) => {
+//   innerCounter++;
+//   if (innerCounter === 1) {
+//     const innerRectangle = document.createElement('DIV');
+//     innerRectangle.setAttribute('class', 'innerRectangle');
+//     rec.appendChild(innerRectangle);
+//   } else {
+//     console.log('this is already taken');
 
-button.addEventListener('click', (event) => {
-  // let rectangle = document.createElement('DIV');
-  makeRectangle(event);
-
-})
-
-const makeRectangle = (event) => {
-  counter++;
-  const container = document.getElementById('happy');
-
-  const rectangle = document.createElement('DIV');
-  rectangle.setAttribute('ID', `${counter}`);
-  rectangle.setAttribute('CLASS', 'rectangle')
-  container.appendChild(rectangle);
-  rectangle.innerText = counter;
-}
 
 
-container.addEventListener('click', (event) => {
-  innerCounter = 0;
-  if (event.target.id !== 'happy') {
-    // alert(event.target.id);
-    const rec = document.getElementById(`${event.target.id}`);
-    makeInnerRectangle(rec)
-    // const innerRectangle = document.createElement('DIV');
-    // innerRectangle.setAttribute('class', 'innerRectangle');
-    // rec.appendChild(innerRectangle);
-  }
-})
 
-const makeInnerRectangle = (rec) => {
-  innerCounter++;
-  if (innerCounter === 1) {
-    const innerRectangle = document.createElement('DIV');
-    innerRectangle.setAttribute('class', 'innerRectangle');
-    rec.appendChild(innerRectangle);
-  } else {
-    console.log('this is already taken');
+
+
+
+
+
   }
 }
